bed4b72dd07544ac7496a91995580256d1c6677e
NoodleFarmer11 <97500413+NoodleFarmer11@users.noreply.github.com>
2024-05-21 11:15:01 +0700

    Update serverstatus-home.js

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -10,8 +10,8 @@
         document.getElementById("serverInd").classList.add("text-green-600");
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Trực Tuyến!&ensp;&middot;&ensp;${data.players.online} đang chơi</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Trực Tuyến, ${data.players.online} đang chơi</p>`;
         {% endif %}
     }
     else {
@@ -23,8 +23,8 @@
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-red-600");
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Đang Bảo Trì...</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Đang Bảo Trì</p>`;
         {% endif %}
     }
 });
